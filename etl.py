00117f45151d9b4064d847c9e34dfe663b41863e
nomanshaikh07 <nomanshaikh9518@gmail.com>
2025-01-02 20:16:21 +0530

    commit

---
+++ etl.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+st.write('Welcome to AVD 123')
+data = {
+    "Task": ["Extract", "Transform", "Load"],
+    "Status": ["Completed", "InProgress", "Pending"]
+}
+df = pd.DataFrame(data)
+st.write("ETL Processing Status : ", df)
